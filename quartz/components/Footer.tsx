b47571ea7f60adaf56abdb7fbc7bd1f89f82b5e5
Vincent Rescigno <vincentrescigno@Vincents-MBP.lan>
2025-06-14 00:16:28 -0400

    Remove Quartz footer branding

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,8 +14,7 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+            © {year} <a href="https://moneyboth.com">MoneyBoth.com</a> — All rights reserved
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
